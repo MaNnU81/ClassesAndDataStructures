9b665b2e113a84762305fd02725f8279baa8fbe7
MaNnu81 <mannuspamm@gmail.com>
2025-05-07 12:52:55 +0200

    ok

--- ClassesAndDataStructures/Program.cs
+++ ClassesAndDataStructures/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace ClassesAndDataStructures
+﻿using System.Collections;
+
+namespace ClassesAndDataStructures
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            Student student1 = new Student("pippo", "de pippis", "male", 2000);
+            Student student2 = new Student("paperoga", "de paperoni", 2000);
+            Student student3 = new("paperoga", "de paperoni", 2000);  //si puo usare anche cosi in quanto è gia dichiarato Student
+            Student student4 = new Student("Qui", "QUA", 2015);
+
+            Teacher teacher1   = new Teacher { Name = "Archimede", Surname = "Pitagorico", Subject = "Math" };
+            Teacher teacher2 = new Teacher { Name = "Pico" }; // solo required
+
+
+            Student[] students = new Student[3];
+            students[0] = student1; 
+            students.Append(student2);
+            students[2] = student3;
+            students.Append(student4);
+
+            ArrayList studentArrayList = new ArrayList();    //solo per retrocompatibilità, da nn usare, puo contenere tutti i tipi.
+            studentArrayList.Add(student1);    
+            studentArrayList.Add(student2);
+            studentArrayList.Add(teacher1);
+
+            List<Student>  studentList = new List<Student>(); //tipizzato come Student accetta solo Student, non Teacher ad esempio
+            studentList.Add(student1);  
+            studentList.Add(student2);
+
+            Hashtable studentHashtable = new Hashtable();   //key value ma accetta tutto
+            studentHashtable.Add("pippo", student1);
+            studentHashtable.Add("pico", teacher1);
+            studentHashtable.Add(5, teacher2);  //addirittura cosi
+
+            Dictionary<string, Student> studentDictionary = new Dictionary<string, Student>();  
+           studentDictionary
+
+
         }
     }
 }
---
+++ ClassesAndDataStructures/Student.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndDataStructures
+{
+    internal class Student
+    {
+        //private string _name;
+        //private string _surname;
+        //private string _gender;
+        //private int _yob;
+
+        //public Student(string name, string surname, int yob)
+        //{
+        //    _name = name;
+        //    _surname = surname;
+        //    _gender = "neutral";
+        //    _yob = yob;
+        //}
+        //public Student(string name, string surname, string gender, int yob)
+        //{
+        //    _name = name;
+        //    _surname = surname; 
+        //    _gender = gender;
+        //    _yob = yob;
+        //}
+
+        public string Name { get;  }  //volendo posso togliere set e si puo solo leggere (get) e non variare(set)
+        public string Surname { get; set; }
+        public string Gender { get; set; }
+
+        private string _name;
+        private string _surname;
+        private string _gender;
+        private int _yob;
+        public int Yob 
+        {
+            get => _yob;
+
+            set
+            {
+                if (value < 1900)
+                {
+                    _yob = 1900;
+                }
+                else
+                {
+                    _yob = value;
+                }
+            }
+                
+         }
+
+        public Student(string name, string surname, int yob)
+        {
+            _name = name;
+            _surname = surname;
+            _gender = "neutral";
+            _yob = yob;
+
+        }
+
+
+        public Student(string name, string surname, string gender, int yob) 
+        {
+            Name = name;
+            Surname = surname;
+            Gender = gender;
+            Yob = yob;
+        }    
+
+    }
+}
